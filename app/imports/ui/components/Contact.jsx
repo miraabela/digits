f9b47212618a3cf7cb5fcffa00ca2899a1edc850
Bela <mirabela@hawaii.edu>
2019-03-25 22:56:42 -1000

    done

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListContacts.jsx. */
 class Contact extends React.Component {
@@ -13,6 +13,9 @@ class Contact extends React.Component {
             <Card.Header>{this.props.contact.firstName} {this.props.contact.lastName}</Card.Header>
             <Card.Meta>{this.props.contact.address}</Card.Meta>
             <Card.Description>{this.props.contact.description}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
           </Card.Content>
         </Card>
     );
